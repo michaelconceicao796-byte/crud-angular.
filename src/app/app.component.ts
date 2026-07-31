a805d4ebbf399c95ec819418e7979ce40c3da136
michaelconceicao796-byte <michaelconceicao796@gmail.com>
2026-07-31 18:07:16 -0300

    crud-angular
    alterado

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MatToolbarModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+
+})
+export class AppComponent {
+   title = ('crud-angular');
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [];
+
+@NgModule({
+  declarations: [],
+
+  imports: [
+    MatToolbarModule,
+    RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
